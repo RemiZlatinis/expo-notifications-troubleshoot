2af472bb59c34359a7ffadfe07557d910c711699
RemiZlatinis <remizlatinis@gmail.com>
2025-03-15 21:19:30 +0200

    Displaying Expo Push Token

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+import { getPushNotificationToken } from "@/services/notifications";
 
 export default function RootLayout() {
+  const [token, setToken] = useState<string>();
+
+  const handleGetExpoPushToken = async () => {
+    const token = await getPushNotificationToken();
+    setToken(token || undefined);
+  };
+
   return (
     <View style={styles.screen}>
-      <Text style={styles.text}>Hello World!</Text>
+      <Text style={styles.text}>{token || "No token"}</Text>
+      <Button title="Get an Expo Push Token" onPress={handleGetExpoPushToken} />
     </View>
   );
 }
